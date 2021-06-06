c9c0a9110d383974a66d0ea5ffd869b9ef98e8d3
Vipul <vipul@Vipuls-iMac.local>
2021-06-06 12:26:09 +0530

    Initial commit

---
+++ app/src/main/java/com/vip/imageslider/ImageSliderFragment.java
@@ -0,0 +1,118 @@
+package com.vip.imageslider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ImageSliderFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ImageSliderFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ImageSliderFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ImageSliderFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ImageSliderFragment newInstance(String param1, String param2) {
+        ImageSliderFragment fragment = new ImageSliderFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_image_slider, container, false);
+    }
+    private ViewPager2 viewPager2;
+    private Handler sliderHandler = new Handler();
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        viewPager2 = view.findViewById(R.id.viewPagerImageSlider);
+
+        List<SliderItems> sliderItems = new ArrayList<>();
+        sliderItems.add(new SliderItems(R.drawable.image1));
+        sliderItems.add(new SliderItems(R.drawable.image2));
+        sliderItems.add(new SliderItems(R.drawable.image3));
+        sliderItems.add(new SliderItems(R.drawable.image4));
+        sliderItems.add(new SliderItems(R.drawable.image2));
+
+        viewPager2.setAdapter(new SliderAdapter(sliderItems,viewPager2));
+
+        viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
+        viewPager2.setOffscreenPageLimit(3);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float r = 1 - Math.abs(position);
+                page.setScaleY(0.85f + r * 0.15f);
+            }
+        });
+
+        viewPager2.setPageTransformer(compositePageTransformer);
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+//                sliderHandler.removeCallbacks(sliderRunnable);
+//                sliderHandler.postDelayed(sliderRunnable, 2000); // slide duration 2 seconds
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/vip/imageslider/SliderItems.java
@@ -0,0 +1,12 @@
+package com.vip.imageslider;
+
+public class SliderItems {
+
+    private int image;
+    SliderItems(int image) {
+        this.image = image;
+    }
+    public int getImage() {
+        return image;
+    }
+}
